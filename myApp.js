0329cf4c21af7ad63e61eedd3c19b54c1f9c8645
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-22 15:01:17 +0000

    11/14 completed in infosec practise

--- myApp.js
+++ myApp.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
 
+
 const app = express();
 app.use(helmet.hidePoweredBy()); //Hide Potentially Dangerous Information
 app.use(helmet.frameguard({action: 'deny'})); //Risk of Clickjacking
 app.use(helmet.xssFilter()); //Risk of Cross Site Scripting (XSS) Attacks
 app.use(helmet.noSniff()); //Avoid Inferring the Response MIME Type
 app.use(helmet.ieNoOpen()); //Prevent IE from Opening Untrusted HTML
+ninetyDaysInSeconds = 90*24*60*60; //Ask Browsers to Access Your Site via HTTPS
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
-
-
-
-
+app.use(helmet.dnsPrefetchControl())//Disable DNS Prefetching
+app.use(helmet.noCache()); //Disable Client-Side Caching
+//Set a Content Security Policy
+app.use(helmet.contentSecurityPolicy(
+  { directives:
+    { defaultSrc: ["'self'"], 
+     scriptSrc: ["'self'", "trusted-cdn.com"] 
+    }}
+));
 
 
 
